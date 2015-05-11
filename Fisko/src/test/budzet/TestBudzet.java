5a7e8b08a2e9331c31527439f7418d37d6cd8514
putnich <mputnikovic@gmx.us>
2015-05-12 01:40:53 +0200

    Testirana klasa Budzet.

---
+++ Fisko/src/test/budzet/TestBudzet.java
@@ -0,0 +1,30 @@
+package test.budzet;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import budzet.Budzet;
+
+public class TestBudzet {
+
+	Budzet b;
+	@Before
+	public void setUp() throws Exception {
+		b = new Budzet();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		b = null;
+	}
+
+	@Test
+	public void testSetter() {
+		b.setIznos(500.0);
+		assertEquals(500.0, b.getIznos(), 0);
+	}
+
+}
